c3256e7bcf0823a2360a4ec898c0aba6aa49d176
p81sunshine <lsnfzj@gmail.com>
2023-10-11 16:51:21 +0800

    Complete server

---
+++ client.cpp
@@ -0,0 +1,317 @@
+#include <iostream>
+
+// #include <winsock2.h>
+#include <stdio.h>
+#include <sys/types.h>
+// #include <windows.h>
+#include <sys/ipc.h>
+#include <arpa/inet.h>
+#include <sys/msg.h>
+#include <netinet/ip.h>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include<unistd.h>
+#include<errno.h>
+
+#include<pthread.h>
+const int MAX_SIZE = 256;
+enum type {CONNECT = 1, DISCONNECT, GET_TIME, GET_NAME, GET_CLIENT_LIST, SEND_MSG, REPOST};
+void *helper_thread(void *arg);
+
+struct Message {
+    long type;
+    char data[MAX_SIZE - 2];
+};
+using namespace std;
+int msg_ID;
+
+class client{
+public:
+    client();
+    ~client();
+    void start();
+private:
+    int client_sd;
+    struct sockaddr_in server_addr;
+    void connecting();
+    void disconnecting();
+    void gettime();
+    void getname();
+    void getlist();
+    void sendmsg();
+    void finish();
+    pthread_t thrd;
+};
+
+client::client(){
+    client_sd = -1;
+    memset(&server_addr, 0, sizeof(server_addr));
+    key_t key = ftok("c", 12);
+    msg_ID = msgget(key, IPC_CREAT | 0666);
+    if (msg_ID < 0){
+        printf("[Error] Message queue create fail: %s\n", strerror(errno));
+        return;
+    }
+}
+
+client::~client(){
+    close(client_sd);
+}
+
+void client::start(){
+    cout << "+----------------------------------+\n";
+    cout << "|   Welcome to use this client!    |\n";
+    while(1){
+        cout << "+----------------------------------+\n";
+        cout << "| 1 : connect to server            |\n";
+        cout << "| 2 : disconnect from server       |\n";
+        cout << "| 3 : get time                     |\n";
+        cout << "| 4 : get server name              |\n";
+        cout << "| 5 : get client list              |\n";
+        cout << "| 6 : send message                 |\n";
+        cout << "| 7 : exit                         |\n";
+        cout << "+----------------------------------+\n";
+        cout << "please enter you choice:";
+        int whichcase;
+        cin >> whichcase;
+        switch (whichcase){
+        case 1:
+            connecting();
+            break;
+        case 2:
+            disconnecting();
+            break;
+        case 3:
+            gettime();
+            break;
+        case 4:
+            getname();
+            break;
+        case 5:
+            getlist();
+            break;
+        case 6:
+            sendmsg();
+            break;
+        case 7:
+            finish();
+            break;
+        default:
+            cout<<"error input"<<endl;
+            break;
+        }
+}
+    }
+
+
+void client::connecting(){
+    cout<<"connecting......"<<endl;
+    //error detection
+    if(client_sd!=-1){
+        cout<<"Error:reconnect"<<endl;
+        return ;
+    }
+    //get socket
+    client_sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sd < 0) {
+        perror("Error: Create socket fail");
+        return;
+    }
+    else{
+        printf("socket success\n");
+    }
+    memset(&server_addr, 0, sizeof(server_addr));
+    //data preparation
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(1733);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    //connection
+    int mark = connect(client_sd, (sockaddr *)&server_addr, sizeof(server_addr));
+    if(mark < 0){
+        printf("[Error] Connect fail, error: %s\n", strerror(errno));
+        return;
+    } else {
+        printf("Connect success\n");
+    }
+    //create pthread
+    if (pthread_create(&thrd, NULL, helper_thread, &client_sd) != 0)
+    {
+        printf("[Error] Create thread fails!\n");
+    }
+}
+
+void client::disconnecting(){
+    //error detection
+    if(client_sd==-1){
+        cout << "Error: No connect" << endl;
+        return ;
+    }
+    //set type
+    char info = DISCONNECT;
+    //send the disconnect request
+    int mark = send(client_sd, &info, sizeof(info), 0);
+    if (mark < 0){
+        printf("[Error] Disconnect send fail, error: %s\n", strerror(errno));
+    }
+    //cancel pthread
+    pthread_cancel(thrd);
+    close(client_sd);
+    //set the state
+    client_sd = -1;
+    printf("[Info] Connection closed!\n");
+}
+
+void *helper_thread(void *arg)
+{
+    int sockfd = *(int *)arg;
+    Message msg;
+    while (1)
+    {
+        memset(&msg, 0, sizeof(msg));
+        //receive the data
+        if (recv(sockfd, &msg, sizeof(msg), 0) < 0){
+            printf("[Error] helper recv fail, error: %s\n", strerror(errno));
+        }
+        //send to the main thread
+        msgsnd(msg_ID, &msg, MAX_SIZE, 0);
+    }
+}
+
+void client::gettime(){
+    //error detection
+    if (client_sd == -1){
+        printf("[Error] No connection detected!\n");
+        return;
+    }
+    //set type
+    char data = GET_TIME;
+    if (send(client_sd, &data, sizeof(data), 0) < 0)
+    {
+        printf("[Error] getServerTime send fail, error: %s\n", strerror(errno));
+        return;
+    }
+    //create massage
+    Message msg;
+    long type = GET_TIME;
+    //receive the message
+    if (msgrcv(msg_ID, &msg, MAX_SIZE, type, 0) < 0)
+    {
+        printf("[Error] getServerTime recv fail, error: %s\n", strerror(errno));
+        return;
+    }
+    //change the data into time
+    time_t t;
+    sscanf(msg.data, "%ld", &t);
+    printf("Time: %s\n", ctime(&t));
+    return;
+}
+
+void client::getname(){
+    //error detection
+    if (client_sd == -1){
+        printf("[Error] No connection detected!\n");
+        return;
+    }
+    //set type
+    char data = GET_NAME;
+    if (send(client_sd, &data, sizeof(data), 0) < 0)
+    {
+        printf("[Error] getServerTime send fail, error: %s\n", strerror(errno));
+        return;
+    }
+    Message msg;
+    long type = GET_NAME;
+    //receive the message
+    if (msgrcv(msg_ID, &msg, MAX_SIZE, type, 0) < 0)
+    {
+        printf("[Error] getServerTime recv fail, error: %s\n", strerror(errno));
+        return;
+    }
+    //output the content
+    printf("Server Name: %s\n", msg.data);
+    return;
+}
+
+void client::getlist(){
+    //error detection
+    if (client_sd == -1){
+        printf("[Error] No connection detected!\n");
+        return;
+    }
+    //set the type
+    char data = GET_CLIENT_LIST;
+    //send data
+    if (send(client_sd, &data, sizeof(data), 0) < 0)
+    {
+        printf("[Error] getServerTime send fail, error: %s\n", strerror(errno));
+        return;
+    }
+    Message msg;
+    long type = GET_CLIENT_LIST;
+    //receive the data
+    if (msgrcv(msg_ID, &msg, MAX_SIZE, type, 0) < 0)
+    {
+        printf("[Error] getServerTime recv fail, error: %s\n", strerror(errno));
+        return;
+    }
+    //output the list
+    printf("Client list: %s\n", msg.data);
+    return;
+}
+
+void client::sendmsg(){
+    //check connenction state
+    if(client_sd == -1){
+        cout<<"error: no connection"<<endl;
+        return ;
+    }
+    //allow user input ip and port
+    char newip[MAX_SIZE];
+    int port;
+    cout<<"please input IP:"<<endl;
+    scanf("%s",newip);
+    in_addr_t ip;
+    ip = inet_addr(newip);
+    cout<<"please input port"<<endl;
+    scanf("%d",&port);
+    Message msg;
+    //set type
+    msg.type = SEND_MSG;
+    sprintf(msg.data, "%u:%d:", ip, port);
+    char content[MAX_SIZE - 50];
+    //read in the content
+    cout<<"please input content"<<endl;
+    scanf("%s", content);
+    sprintf(msg.data + strlen(msg.data), "%s", content);
+    printf("%s\n", msg.data);
+    if (send(client_sd, &msg, sizeof(msg), 0) < 0)
+    {
+        printf("[Error] sendToOtherClient send fail, error: %s\n", strerror(errno));
+        return;
+    }
+    memset(&msg, 0, sizeof(msg));
+    long type = SEND_MSG;
+    if (msgrcv(msg_ID, &msg, MAX_SIZE, type, 0) < 0)
+    {
+        printf("[Error] sendToOtherClient recv fail, error: %s\n", strerror(errno));
+        return;
+    }
+
+    printf("Server Response: %s\n", msg.data);
+    return;    
+}
+
+void client::finish(){
+    //check the connection state
+    if(client_sd!=-1){
+        disconnecting();
+    }
+    exit(0);
+}
+
+int main(){
+    client c;
+    c.start();
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,210 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <mutex>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <string.h>
+#define STRINGIFY(x) #x
+
+
+const int MAXCLIENT = 10;
+struct clientInfo {
+    int clientSocket; // handle
+    sockaddr_in clientAddr; // Socket address, which consists of IP and port
+    int status; 
+};
+struct clientInfo clientList[MAXCLIENT];
+int clientIndex = 0;
+
+/* Set datapack */
+enum msgType {CONNECT = 1, DISCONNECT, GET_TIME, GET_NAME, GET_CLIENT_LIST, SEND_MSG, REPOST};
+
+const int MAX_SIZE = 256;
+
+struct Message {
+    long type;
+    char data[MAX_SIZE - 2];
+};
+
+void *handle_client(void* client_socket){
+    int clientSocket = *(int*)client_socket;
+    char buffer[] = "Hello, I am server";
+    send(clientSocket, buffer, sizeof(buffer), 0);
+    struct Message msg;
+    memset(&msg, 0, sizeof(msg));
+    while (true) {
+        /* https://www.ibm.com/docs/en/zos/2.4.0?topic=functions-recv-receive-data-socket*/
+        int recv_len = recv(clientSocket, &msg, sizeof(msg), 0);
+        printf("%d\n", recv_len);
+        if (recv_len < 0) {
+            printf("Fail to receive message from client %d, error code: %d\n", clientSocket, errno);
+            continue;
+        } else if (recv_len == 0) {
+            printf("Client %d close connection!\n", clientSocket);
+            break;
+        }
+        printf("+----------------------------------------------------------+\n");
+        printf("Receive Data Package from client: %d\n", clientSocket);
+
+        if(msg.type == DISCONNECT) { // Loop exit
+            printf("Type:DISCONECT\n");
+            close(clientSocket);
+            printf("Client %d disconnected\n", clientSocket);
+            for (int i = 0; i < clientIndex; i++) {
+                if (clientList[i].clientSocket == clientSocket) {
+                    clientList[i].status = 0;
+                    /* Remove the client from the list*/
+                    for (int j = i; j < clientIndex - 1; j++) {
+                        clientList[j] = clientList[j + 1];
+                    }
+                }
+            }
+            printf("Success to remove client %d from list\n", clientSocket);
+            break;
+        } else if (msg.type == GET_TIME) {
+            printf("Type:GET_TIME\n");
+            time_t t;
+            time(&t);
+            struct Message msg_ret;
+            sprintf(msg_ret.data, "%ld", t);
+            msg_ret.type = GET_TIME;
+            if (send(clientSocket, &msg_ret, sizeof(msg_ret), 0) < 0) {
+                printf("Fail to send message to client %d, error code: %d\n", clientSocket, errno);
+            } else {
+                printf("Success to send time %s to client %d\n",ctime(&t), clientSocket);
+            }
+
+        printf("+----------------------------------------------------------+\n");
+
+        } else if (msg.type == GET_NAME) {
+            printf("Type:GET_NAME\n");
+            Message msg_ret;
+            gethostname(msg_ret.data, sizeof(msg_ret.data));
+            msg_ret.type = GET_NAME;
+            if (send(clientSocket, &msg_ret, sizeof(msg_ret), 0) < 0) {
+                printf("Fail to send message to client %d, error code: %d\n", clientSocket, errno);
+            } else {
+                printf("Success to send name %s to client %d\n", msg_ret.data, clientSocket);
+            }
+        printf("+----------------------------------------------------------+\n");
+
+        } else if (msg.type == GET_CLIENT_LIST) {
+            printf("Type:GET_CLIENT_LIST\n");
+            Message msg_ret;
+            char buffer[MAX_SIZE];
+            sprintf(buffer, "Client list: \n");
+            for (int i = 0; i < clientIndex; i++) {
+                sprintf(buffer, "%s%d: address: %s:%d\n", buffer, i + 1, inet_ntoa(clientList[i].clientAddr.sin_addr), ntohs(clientList[i].clientAddr.sin_port)); // Question
+            }
+            sprintf(msg_ret.data, "%s", buffer); // Copy buffer to msg_ret.data
+            msg_ret.type = GET_CLIENT_LIST;
+            if (send(clientSocket, &msg_ret, sizeof(msg_ret), 0) < 0) {
+                printf("Fail to send message to client %d, error code: %d\n", clientSocket, errno);
+            } else {
+                printf("Success to send client list to client %d\n", clientSocket);
+            }
+            printf("+----------------------------------------------------------+\n");
+        } else if (msg.type == SEND_MSG) {
+            printf("Type:SEND_MSG\n");
+            in_addr_t ip;
+            in_port_t port;
+            char content[MAX_SIZE - 50];
+            memset(content, 0, sizeof(content));
+            sscanf(msg.data, "%u:%hu:%s", &ip, &port, content);
+            port = htons(port);
+            struct in_addr addr;
+            addr.s_addr = ip;
+            printf("Client send ip:%s, port:%u, content:%s\n", inet_ntoa(addr), port, content);
+            int targetSocket = -1;
+            for (int i = 0; i < clientIndex; i++) {
+                if (clientList[i].clientAddr.sin_addr.s_addr == ip && clientList[i].clientAddr.sin_port == port) {
+                    targetSocket = i;
+                    break;
+                }
+            }
+            Message msg_ret;
+            msg_ret.type = SEND_MSG;
+            if (targetSocket == -1) {
+                printf("Fail! Cliend sending address is not connected!\n");
+                sprintf(msg_ret.data, "Fail to send message to client %s:%u\n", inet_ntoa(addr), port);
+            } else {
+                Message msg_send;
+                msg_send.type = REPOST; 
+                sprintf(msg_send.data, "%s", content);
+                sprintf(msg_ret.data, "%s", "Forwad Success!\n");
+                if (send(clientList[targetSocket].clientSocket, &msg_send, sizeof(msg_send), 0) < 0) {
+                    printf("Fail to Forward message to client %d, error code: %d\n", clientList[targetSocket].clientSocket, errno);
+                } else {
+                    printf("Success to Forward message to client %d\n", clientSocket);
+                }
+            }
+            if (send(clientSocket, &msg_ret, sizeof(msg_ret), 0) < 0) {
+                printf("Fail to send message to client %d, error code: %d\n", clientSocket, errno);
+            } else {
+                printf("Success to return success forward signal to  %d\n", clientSocket);
+            }
+            printf("+----------------------------------------------------------+\n");
+        } else {
+            printf("Unknown message type %ld\n", msg.type);
+            printf("+----------------------------------------------------------+\n");
+        }
+        memset(&msg, 0, sizeof(msg));
+    }
+    return NULL;
+}
+
+int main() {
+    /*
+    * socket()的参数含义如下:
+    * AF_INET - 使用IPv4协议
+    * SOCK_STREAM - 使用面向连接的TCP协议
+    * 0 - 使用默认的网络协议(IPPROTO_IP for AF_INET)
+    * 所以这里用socket()函数创建了一个使用IPv4和TCP协议的socket
+    */
+    /* SocketFD file descriptor*/
+    int socketDestination = socket(AF_INET, SOCK_STREAM, 0);
+    /* Set server listen address*/
+    if (socketDestination == -1) {
+        printf("Fail to create socket\n");
+        exit(EXIT_FAILURE); 
+    } else {
+        printf("Success to create socket\n");
+    }
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(1733); // 使用学号后4位作为端口
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    /* Bind the address to socket*/
+    bind(socketDestination, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    printf("Success to bind address to socket\n");
+    listen(socketDestination, MAXCLIENT);
+    printf("Success to listen socket\n");
+
+
+    while(true) {
+        struct sockaddr_in clientAddr;
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        /*
+        It extracts the first connection request 
+        on the queue of pending connections for 
+        the listening socket, sockfd, creates a 
+        new connected socket, 
+        and returns a new file descriptor referring to that socket
+        */
+        printf("Waiting for client connection...\n");
+        int clientSocket = accept(socketDestination, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        clientList[clientIndex].clientSocket = clientSocket;
+        clientList[clientIndex].clientAddr = clientAddr;
+        clientList[clientIndex++].status = 1;
+        printf("Success to accept client connection handle: %d, address: %s, port: %d\n", clientSocket, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+        pthread_t conn_thread;
+        pthread_create(&conn_thread, NULL, handle_client, &clientSocket);
+    }
+}
